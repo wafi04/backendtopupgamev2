518a8aec5590621d1a1e982f46fa4fe230a76ac1
wafi04 <wafi3040@gmail.com>
2025-04-15 11:39:28 +0700

    bismillah

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Express } from 'express';
 import cookieParser from 'cookie-parser';
 import router from './routes';
+import 'module-alias/register';
 
 const app: Express = express();
 const port = process.env.PORT || 4000
